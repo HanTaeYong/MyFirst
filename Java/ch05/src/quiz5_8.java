480b02c87610aafda4441aa51e86e434b18ba8e3
HanTaeYong1 <hty1048@naver.com>
2018-06-22 19:33:10 +0900

    no message

--- Java/ch05/src/quiz5_8.java
+++ Java/ch05/src/quiz5_8.java
@@ -2,38 +2,19 @@ import java.util.*;
 public class quiz5_8 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int num;
+		int sum = 0;
 		
 		System.out.println("5개 정수 입력");
-		int a = sc.nextInt();
-		if(a<1) {
-			System.out.println("재입력.");
-			a = sc.nextInt();
+		for(int i=0; i<5; i++) {
+			num = sc.nextInt();
+			if(num<1) {
+				System.out.println("재입력");
+				i--;
+				continue;
+			}
+			sum+=num;
 		}
-		
-		int b = sc.nextInt();
-		if(b<1) {
-			System.out.println("재입력.");
-			b = sc.nextInt();
-		}
-		
-		int c = sc.nextInt();
-		if(c<1) {
-			System.out.println("재입력.");
-			c = sc.nextInt();
-		}
-		
-		int d = sc.nextInt();
-		if(d<1) {
-			System.out.println("재입력.");
-			d = sc.nextInt();
-		}
-		
-		int e = sc.nextInt();
-		if(e<1) {
-			System.out.println("재입력.");
-			e = sc.nextInt();
-		}
-		
-		System.out.println("합 = " + (a+b+c+d+e));;
+		System.out.println("합 = " + sum);;
 	}
 }
